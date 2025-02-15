188b05feacb49ebdbf82b1162bb7b14982c6bef5
marycieloSanchez1 <72656839@certus.edu.pe>
2025-02-15 17:37:09 -0500

    perfil1

--- components/ReservarPsiPreview.tsx
+++ components/ReservarPsiPreview.tsx
@@ -3,6 +3,7 @@ import { Avatar, AvatarImage } from "@/components/ui/avatar";
 import {
   Card,
   CardContent,
+  CardHeader,
   CardDescription,
   CardFooter,
   CardTitle,
@@ -29,11 +30,7 @@ interface psicologo {
   link: string;
   flag: string;
 }
-const profiles =[
-  {
-      id:"/profile",
-    },
-  ]
+
 export default function ReservarPsiPreview({
   psicologo,
 }: {
@@ -94,38 +91,79 @@ export default function ReservarPsiPreview({
             >
               Ver Perfil
             </Button>
-
           </CardFooter>
         </CardContent>
       </Card>
-                    
-      <Modal isOpen={isOpen} onOpenChange={onOpenChange}
-       backdrop="opaque"
-       classNames={{
-         body: "py-6",
-         backdrop: "bg-[#d8dceb]/50 backdrop-opacity-40",
-         base: "border-[#d8dceb] bg-[#ffffff] dark:bg-[#ffffff] text-[#a8b0d3]",
-         header: "border-b-[1px] border-[#d8dceb]",
-         footer: "border-t-[1px] border-[#d8dceb]",
-         closeButton: "hover:bg-white/5 active:bg-white/10",
-       }}
-      
+
+      <Modal
+        isOpen={isOpen}
+        onOpenChange={onOpenChange}
+        size={"2xl"}
+        backdrop="opaque"
+        classNames={{
+          body: "py-6",
+          backdrop: "bg-[#d8dceb]/50 backdrop-opacity-40",
+          base: "border-[#d8dceb] bg-[#ffffff] dark:bg-[#ffffff] text-[#a8b0d3]",
+          header: "border-b-[1px] border-[#d8dceb]",
+          footer: "border-t-[1px] border-[#d8dceb]",
+          closeButton: "hover:bg-white/5 active:bg-white/10",
+          
+        }}
       >
         <ModalContent>
           {(onClose) => (
             <>
-              <div className="grid grid-cols-4 gap-4">
-                <div className="col-span-2">
-                  <Image
-                    src={psicologo.img}
-                    alt={psicologo.name}
-                    width={200}
-                    height={400}
-                    className="object-cover"
-                  />
+              <ModalContent className="w-[695px] h-[416px] bg-background rounded-3xl  overflow-hidden  mt-8">
+                <div className="grid grid-cols-[0.8fr_1.6fr] items-center">
+                  <div className="h-full w-full flex ">
+                    <Avatar className="w-[208px] h-[416px] rounded-2xl overflow-hidden">
+                      <AvatarImage
+                        src={"https://github.com/shadcn.png"}
+                        className="w-full h-full object-cover"
+                      />
+                    </Avatar>
+                  </div>
+
+                  <div className="text-[#634AE2] text-start ">
+                    <ModalHeader className="space-y-1 px-1">
+                      <div className="text-[#634AE2] text-2xl font-semibold">
+                        Nombre Apellidos
+                      </div>
+                    </ModalHeader>
+
+                    <ModalBody className="py-2 px-1 gap-0.5">
+                      <p className="text-[#634AE2] font-normal text-base">
+                        Especialidades:
+                      </p>
+                      <div className="flex gap-2 mt-1.5 mb-1">
+                        <span className="px-3 py-1 bg-[#E7E7FF] text-[#634AE2] rounded-xl text-sm">
+                          TDAH
+                        </span>
+                        <span className="px-3 py-1 bg-[#E7E7FF] text-[#634AE2] rounded-xl text-sm">
+                          Dislexia
+                        </span>
+                        <span className="px-3 py-1 bg-[#E7E7FF] text-[#634AE2] rounded-xl text-sm">
+                          Aprendizaje
+                        </span>
+                      </div>
+                      <p className="text-[#634AE2] text-sm  leading-[22px] content-normal mr-1">
+                        Licenciada Psicopedagogía con una amplia experiencia en
+                        estimulación cognitiva, orientación vocacional, técnicas
+                        de estudio y estrategias para mejorar memoria, atención
+                        y habilidades cognitivas. Además, brinda acompañamiento
+                        a familias y docentes para potenciar el desarrollo
+                        integral en cada etapa de la vida.
+                      </p>
+                    </ModalBody>
+
+                    <ModalFooter className="flex justify-start px-1">
+                      <Button className="rounded-3xl bg-[#634AE2] text-white px-6 text-sm font-normal ">
+                        Agendar
+                      </Button>
+                    </ModalFooter>
+                  </div>
                 </div>
-                <div className="col-span-2"></div>
-              </div>
+              </ModalContent>
             </>
           )}
         </ModalContent>
