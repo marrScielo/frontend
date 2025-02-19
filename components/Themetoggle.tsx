3d53c0cc590a94a790171e4f5b2340f16ee6fe69
FranksVilca <franksjaviervilcaquispe@gmail.com>
2025-02-19 11:02:00 -0500

    solucionado thema

--- components/Themetoggle.tsx
+++ components/Themetoggle.tsx
@@ -1,39 +1,45 @@
 "use client";
 import * as React from "react";
-import { Moon, Sun } from   "lucide-react";
 import { useTheme } from "next-themes";
-
-import { Button } from "@/components/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
+import { Icons } from "@/icons";
 
 export function ThemeToggle() {
-  const { setTheme } = useTheme();
+  const { setTheme, theme } = useTheme();
+  const toggleTheme = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  };
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")}>
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
-          Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
-          System
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <div>
+      <label className="relative inline-block w-16 h-8">
+        <input
+          type="checkbox"
+          className="opacity-0 w-0 h-0"
+          checked={theme === "light" }
+          onChange={toggleTheme}
+        />
+        <span
+          className={`absolute h-[33px] cursor-pointer top-0 left-0 right-0 bottom-0 transition-all duration-1500 rounded-full ${
+            theme === "light"  ? "bg-[#9494F3]" : "bg-[#9494F3]"
+          }`}
+        >
+          <span
+            className={`absolute h-[33px] w-[33px] bg-[#634AE2] rounded-full transition-all duration-1500  ${
+              theme === "light"  ? "translate-x-8" : "translate-x-0"
+            }`}
+          ></span>
+        </span>
+
+        <span
+          className="sun absolute top-1/2 right-[2px] z-10 w-6 h-6 transition-all duration-1500  transform -translate-y-1/2"
+          dangerouslySetInnerHTML={{ __html: Icons.sun }}
+        ></span>
+
+        <span
+          className="moon absolute top-1/2 left-2 z-10 w-6 h-6 transition-all duration-1500 transform -translate-y-1/2"
+          dangerouslySetInnerHTML={{ __html: Icons.moon }}
+        ></span>
+      </label>
+    </div>
   );
 }
