35021448fe00bdac4bdd2969aad03b1718ed448c
Toowaa <brahanax@gmail.com>
2025-02-21 11:39:33 -0500

    Boton de cerrar sesion

--- app/user/blog/page.tsx
+++ app/user/blog/page.tsx
@@ -1,11 +1,15 @@
 'use client'
 
 import BlogUsuarioCrear from "@/components/blogCrear/blogUsuarioCrear"
+import CerrarSesion from "@/components/CerrarSesion"
 
 export default function Blog() {
     return (
         <div>
+            <div className="flex justify-between items-center w-full">
             <h1 className=" font-bold text-[32px] leading-[40px] ml-11 mb-5 mt-10 text-[#634AE2]  " >Blog</h1>
+            <CerrarSesion />
+            </div>
             <BlogUsuarioCrear />
         </div>
     )
