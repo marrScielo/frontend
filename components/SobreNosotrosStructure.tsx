234b8b5bb446a1bd6884b0b5861a94ecbc9c175a
marycieloSanchez1 <72656839@certus.edu.pe>
2025-02-24 11:16:55 -0500

    2

--- components/SobreNosotrosStructure.tsx
+++ components/SobreNosotrosStructure.tsx
@@ -2,43 +2,69 @@ import { QuienesSomos } from "@/interface";
 
 export default function SobreNosotrosStructure({ qs }: { qs: QuienesSomos[] }) {
   return (
-    <div className="w-2/5">
+    <div className="w-full">
       {qs.map((item, index) => (
-        <div key={index} className="">
-          <div className="">
-            <div className="pt-8 ml-20 text-[#fff] pb-24">
-              <section >
-                <h2 className="text-5xl font-bold mb-3">¿Quiénes Somos?</h2>
-                <p className="font-extralight text-lg pt-3">{item.quienesSomos}</p>
-              </section>
-  
-              <section className="pt-7">
-                <h3 className="font-semibold text-2xl">Misión</h3>
-                <p className="font-extralight text-lg pt-2">{item.mision}</p>
-              </section>
+        <div key={index} className="text-center ">
+          <div className="pt-8  text-[#fff]  ">
+            <section>
+              <h2 className="text-5xl font-bold mb-3">¿Quiénes Somos?</h2>
+              <p
+                className="font-extralight text-lg pt-3"
+                dangerouslySetInnerHTML={{ __html: item.quienesSomos }}
+              />
+            </section>
 
-              <section className="pt-7">
-                <h3 className="font-semibold text-2xl">Visión</h3>
-                <p className="font-extralight text-lg pt-2">{item.vision}</p>
-              </section>
+            <section className="pt-7">
+              <h3 className="font-semibold text-2xl">Misión</h3>
+              <p
+                className="font-extralight text-lg pt-2"
+                dangerouslySetInnerHTML={{ __html: item.mision }}
+              />
+            </section>
 
-              <section className="pt-20">
-                <h3 className="font-medium text-3xl">Valores de Marca</h3>
-                <div className="pt-7">
-                  <h4 className="font-semibold text-2xl">Empatía</h4>
-                  <p className="font-extralight text-lg pt-2">{item.valor1}</p>
-                </div>
-                <div className="pt-7">
-                  <h4 className="font-semibold text-2xl">Confianza</h4>
-                  <p className="font-extralight text-lg pt-2">{item.valor2}</p>
-                </div>
-                <div className="pt-7">
-                  <h4 className="font-semibold text-2xl">Profesionalismo</h4>
-                  <p className="font-extralight text-lg pt-2">{item.valor3}</p>
-                </div>
-              </section>
-            </div>
+            <section className="pt-7">
+              <h3 className="font-semibold text-2xl">Visión</h3>
+              <p
+                className="font-extralight text-lg pt-2"
+                dangerouslySetInnerHTML={{ __html: item.vision }}
+              />
+            </section>
+
+            <section className="pt-20">
+              <h3 className="font-medium text-3xl">Valores de Marca</h3>
+              <div className="pt-7">
+                <h4 className="font-semibold text-2xl">Empatía</h4>
+                <p
+                  className="font-extralight text-lg pt-2"
+                  dangerouslySetInnerHTML={{ __html: item.valor1 }}
+                />
+              </div>
+              <div className="pt-7">
+                <h4 className="font-semibold text-2xl">Confianza</h4>
+                <p
+                  className="font-extralight text-lg pt-2"
+                  dangerouslySetInnerHTML={{ __html: item.valor2 }}
+                />
+              </div>
+              <div className="pt-7">
+                <h4 className="font-semibold text-2xl">Profesionalismo</h4>
+                <p
+                  className="font-extralight text-lg pt-2"
+                  dangerouslySetInnerHTML={{ __html: item.valor3 }}
+                />
+              </div>
+            </section>
           </div>
+          <div
+            className="w-full h-full pt-[75vh]"
+            style={{
+              backgroundImage: `linear-gradient(to right, #3616D87A 0%, #3616D870 30%, #7863E37A 70%, #7863E370 100%),url('/AboutUs/familiafeliz.webp')`,
+              backgroundColor: "#fff",
+              backgroundSize: "cover",
+              backgroundPosition: " center",
+              backgroundRepeat: "no-repeat",
+            }}
+          ></div>
         </div>
       ))}
     </div>
