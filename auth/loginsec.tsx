da2ef846272b7051d6b3755fe0393fc976fabcfe
marycieloSanchez1 <72656839@certus.edu.pe>
2025-02-26 10:19:31 -0500

    as

--- auth/loginsec.tsx
+++ auth/loginsec.tsx
@@ -19,15 +19,9 @@ interface AuthState {
       setAuthState({ ...authState, loading: true });
       try {
         const response = await fetch(
-<<<<<<< HEAD
-          //loCAL 
-          //"http://127.0.0.1:8000/api/auth/login", 
 
-          //HOSTINGUE BACKEND 
-          "https://back.contigo-voy.com/api/auth/login",
-=======
-          `${process.env.NEXT_PUBLIC_API_URL}api/auth/login`,
->>>>>>> 4eb9cbb0f65b172437334b0aa3e21936df005abc
+          'http://127.0.0.1:8000/api/auth/login',
+
           {
             method: "POST",
             headers: {
