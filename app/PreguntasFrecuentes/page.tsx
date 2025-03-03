a9128df191ff75c5d1e831e9b21ff1e5b7e45f16
marycieloSanchez1 <72656839@certus.edu.pe>
2025-03-03 11:16:21 -0500

    avance 4

--- app/PreguntasFrecuentes/page.tsx
+++ app/PreguntasFrecuentes/page.tsx
@@ -12,10 +12,10 @@ export default function App() {
     {
       Question: "¿Cuánto va a costar mi terapia?",
       Answer:
-        "El costo de las terapias varía según el tipo de terapia que el paciente busque. Los precios varían entre 60 y 120 soles por sesión.",
+        "El costo de las terapias varía según el tipo de terapia que el paciente busque. Los precios varían entre 69 y 129 soles por sesión.",
     },
     {
-      Question: "¿Cuál es el tiempo de duración de la consulta?",
+      Question: "¿Cuánto es el tiempo de duración de la consulta?",
       Answer:
         "Cada persona es única, por lo que no podemos estimar el tiempo ni el costo sin la consulta. Allí, el especialista brindará un diagnóstico preciso y personalizará tu terapia para garantizar los mejores resultados.",
     },
@@ -26,11 +26,6 @@ export default function App() {
     },
     {
       Question: "¿Qué tipos de terapia manejan los psicólogos?",
-      Answer:
-        "Nuestros especialistas están altamente capacitados en terapia cognitivo-conductual, uno de los enfoques más efectivos y respaldados científicamente en la actualidad para el tratamiento de diversas condiciones psicológicas.",
-    },
-    {
-      Question: "¿Qué tipos de terapia manejan los psicólogos 2?",
       Answer:
         "Nuestros especialistas están altamente capacitados en terapia cognitivo-conductual, uno de los enfoques más efectivos y respaldados científicamente en la actualidad para el tratamiento de diversas condiciones psicológicas.",
     },
