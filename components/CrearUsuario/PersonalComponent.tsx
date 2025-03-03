b991ce92138e35398ca697bded51669adbef4b71
Toowaa <brahanax@gmail.com>
2025-03-03 11:40:50 -0500

    Refact:Solid CrearUsuario

---
+++ components/CrearUsuario/PersonalComponent.tsx
@@ -0,0 +1,67 @@
+"use client";
+import type { FormData } from "@/interface";
+import React from "react";
+import { getLocalTimeZone, today } from "@internationalized/date";
+import { Suceesfully } from "./SuccesFull";
+import { PersonalForm } from "./DataFormularios";
+import { DataView } from "./DataView";
+
+export const initialFormState: FormData = {
+  name: "",
+  apellido: "",
+  fecha_nacimiento: today(getLocalTimeZone()),
+  edad: 0,
+  genero: "",
+  pais: "",
+  email: "",
+  password: "",
+  introduccion: "",
+  imagen: "",
+  experiencia: 0,
+  especialidades: [1, 3],
+  horario: {
+    Lunes: [["09:00", "12:00"]],
+    Martes: [["10:00", "15:00"]],
+    Miercoles: [["12:00", "16:00"]],
+  },
+};
+export default function PersonalComponent() {
+  const [currentView, setCurrentView] = React.useState<"form" | "data">("form");
+  const [formData, setFormData] = React.useState<FormData>(initialFormState);
+
+  const handleNext = (data: any) => {
+    setFormData(data); 
+    setCurrentView("data"); 
+  };
+
+  const handleBack = () => {
+    setCurrentView("form"); 
+  };
+
+  const [IsSend, setIsSend] = React.useState(false);
+
+  const resetForm = () => {
+    setCurrentView("form");
+    setIsSend(false);
+    setFormData(initialFormState);
+  };
+
+  return (
+    <>
+      {IsSend === false ? (
+        currentView === "form" ? (
+          <PersonalForm onNext={handleNext} initialFormData={formData} />
+        ) : (
+          <DataView
+            formData={formData}
+            onBack={handleBack}
+            setFormData={setFormData}
+            setIsSend={setIsSend}
+          />
+        )
+      ) : (
+        <Suceesfully setIsSend={() => resetForm()} />
+      )}
+    </>
+  );
+}
