63f2e03a8de7b44737e964d0a0be3080f6ce1504
Toowaa <brahanax@gmail.com>
2025-03-05 18:10:09 -0500

    fix(warning):fixed chrome warning image

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,9 @@
 "use client";
 import Link from "next/link";
 import React from "react";
-import Image from "next/image";
+
 import { DesktopNav } from "./DesktopNav";
+import { Image } from "@heroui/react";
 
 const navItems = [
   {
@@ -43,10 +44,8 @@ const Navbar = () => {
   return (
     <nav className="bg-background h-[10vh] flex items-center fixed w-full z-10 top-0">
       <div className="w-full p-6 flex items-center justify-between">
-        <Link href="/">
-          <h1 className="font-normal text-3xl pl-10">
-            <Image src="/LOGO.webp" alt="logo" width={150} height={100} />
-          </h1>
+        <Link href="/" className="pl-10 z-0">
+          <Image src="/LOGO.webp" alt="log" width={150} height={50} />
         </Link>
         <div className="flex items-center gap-x-5">
           <DesktopNav navItems={navItems} />
--- components/main/chooseUs.tsx
+++ components/main/chooseUs.tsx
@@ -1,78 +1,44 @@
 "use client";
 
+import { Image } from "@heroui/react";
 import { motion } from "framer-motion";
-import Image from "next/image";
+
 
 export default function ChooseUs() {
-  const features = [
+
+  const feat = [
     {
-      icon: (
-        <Image
-          src={"/ChooseUsImages/especialistas.webp"}
-          alt="especialista"
-          width={70}
-          height={60}
-          
-        />
-      ),
+      icon: "/ChooseUsImages/especialistas.webp",
+      alt: "especialista",
       title: "Especialistas colegiados",
     },
     {
-      icon: (
-        <Image
-          src="/ChooseUsImages/atencionvirtual.webp"
-          alt="Atención virtual"
-          width={70}
-          height={60}
-        />
-      ),
+      icon: "/ChooseUsImages/atencionvirtual.webp",
+      alt: "Atención virtual",
       title: "Atención virtual",
     },
     {
-      icon: (
-        <Image
-          src="/ChooseUsImages/etica.webp"
-          alt="Ética y confiabilidad"
-          width={70}
-          height={60}
-        />
-      ),
+      icon: "/ChooseUsImages/etica.webp",
+      alt: "Ética y confiabilidad",
       title: "Ética y confiabilidad",
     },
     {
-      icon: (
-        <Image
-          src="/ChooseUsImages/horarios.webp"
-          alt="Horarios flexibles"
-          width={70}
-          height={60}
-        />
-      ),
+      icon: "/ChooseUsImages/horarios.webp",
+      alt: "Horarios flexibles",
       title: "Horarios flexibles",
     },
     {
-      icon: (
-        <Image
-          src="/ChooseUsImages/costos.webp"
-          alt="Costos accesibles"
-          width={70}
-          height={60}
-        />
-      ),
+      icon: "/ChooseUsImages/costos.webp",
+      alt: "Costos accesibles",
       title: "Costos accesibles",
     },
     {
-      icon: (
-        <Image
-          src="/ChooseUsImages/confidencialidad.webp"
-          alt="Confidencialidad"
-          width={70}
-          height={60}
-        />
-      ),
+      icon: "/ChooseUsImages/confidencialidad.webp",
+      alt:"Confidencialidad",          
       title: "Confidencialidad",
     },
   ];
+
   const containerVariants = {
     hidden: { opacity: 0 },
     visible: {
@@ -119,26 +85,27 @@ export default function ChooseUs() {
         className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-40 justify-items-center"
       >
         {/* Primer fila con los primeros 4 elementos */}
-        {features.slice(0, 4).map((feature, index) => (
+        {feat.slice(0, 4).map((feature, index) => (
           <motion.div
             key={index}
             variants={itemVariants}
             whileHover={{ scale: 1.05 }}
             className="group"
           >
-           <div className="flex flex-col items-center justify-center w-40 h-40 rounded-full bg-[#634AE2] backdrop-blur-sm transition-all duration-300 cursor-pointer ">
-                <div className="p-4 bg-[#634AE2] rounded-full group-hover:bg-[#9494F3] transition-colors duration-300">
-                  {feature.icon}
-                </div>
+            <div className="flex flex-col items-center justify-center w-40 h-40 rounded-full bg-[#634AE2] backdrop-blur-sm transition-all duration-300 cursor-pointer ">
+              <div className="p-4 bg-[#634AE2] rounded-full group-hover:bg-[#9494F3] transition-colors duration-300">
+                <Image src={feature.icon} alt={feature.alt} width={70} height={60} />
+             
               </div>
-              <h3 className="text-center text-lg w-40 font-semibold text-title   mt-3 ">
-                {feature.title}
-              </h3>
+            </div>
+            <h3 className="text-center text-lg w-40 font-semibold text-title   mt-3 ">
+              {feature.title}
+            </h3>
           </motion.div>
         ))}
 
         <div className="lg:col-span-4 flex justify-center gap-40">
-          {features.slice(4).map((feature, index) => (
+          {feat.slice(4).map((feature, index) => (
             <motion.div
               key={index}
               variants={itemVariants}
@@ -147,7 +114,7 @@ export default function ChooseUs() {
             >
               <div className="flex flex-col items-center justify-center w-40 h-40 rounded-full bg-[#634AE2] backdrop-blur-sm transition-all duration-300 cursor-pointer shadow-lg space-y-4">
                 <div className="p-4 bg-[#634AE2] rounded-full group-hover:bg-[#9494F3] transition-colors duration-300">
-                  {feature.icon}
+                  <Image src={feature.icon} alt={feature.alt} width={70} height={60} />
                 </div>
               </div>
               <h3 className="text-center text-lg w-full font-semibold text-title   mt-3 ">
