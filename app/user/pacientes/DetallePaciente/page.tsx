57aedf496d63966b639281da92635e599d703e75
FranksVilca <franksjaviervilcaquispe@gmail.com>
2025-03-12 12:09:10 -0500

    feat(User/Pacientes): añadido el navBar

---
+++ app/user/pacientes/DetallePaciente/page.tsx
@@ -0,0 +1,48 @@
+"use client";
+import React from "react";
+import DashboardComponents from "@/components/User/Dashboard/DashboardComponents";
+import CerrarSesion from "@/components/CerrarSesion";
+import { Icons } from "@/icons";
+import NavbarPaciente from "@/components/User/Pacientes/NavbarPaciente";
+
+const PageHome = () => {
+  return (
+    <div className="pb-8 bg-[#eaeded]">
+      {/* Header */}
+      <div className="flex flex-1 bg-[#eaeded] w-full z-30 mt-4">
+        <div>
+          <nav className="bg-[#eaeded] rounded-2xl mt-3 h-[12vh] flex items-center w-[calc(95vw-270px)] p-4">
+            <div className="bg-[#eaeded] flex items-start justify-between w-full">
+              <div className="flex justify-between gap-5">
+                <button>
+                  <span
+                    dangerouslySetInnerHTML={{
+                      __html: Icons.arrow.replace(
+                        /<svg /,
+                        '<svg fill="#634AE2" transform="rotate(90)" width="2em" height="2em" stroke="#634AE2" stroke-width="2" '
+                      ),
+                    }}
+                  />
+                </button>
+                <h1 className="text-4xl font-bold text-[#634AE2]">
+                  Manuel Peres #1{" "}
+                </h1>
+                <button className="bg-[#634AE2] text-[#fff] rounded-full text-base px-4 py-2 font-normal">
+                  Nueva Cita
+                </button>
+              </div>
+              <div className="flex gap-x-2 mt-2">
+                <CerrarSesion />
+              </div>
+            </div>
+          </nav>
+        </div>
+      </div>
+      {/* Navbar */}
+      <NavbarPaciente/>
+      
+    </div>
+  );
+};
+
+export default PageHome;
---
+++ components/User/Pacientes/NavbarPaciente.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Icons } from "@/icons";
+
+const NavbarPaciente = () => {
+  return (
+    <div>
+      <div className="flex w-full mt-8 ">
+        <div className="bg-[#6364F4] w-full h-[8vh] flex flex-row justify-start items-center px-4">
+          <div className="flex flex-row gap-4">
+            <div className="w-full max-w-xl flex flex-row gap-4 justify-between">
+              <button className="text-[#fff] rounded-full hover:bg-[#fff] hover:text-[#6364F4] px-4 py-2">
+                Datos Personales
+              </button>
+              <button className="text-[#fff] rounded-full hover:bg-[#fff] hover:text-[#6364F4] px-4 py-2">
+                Historial Clinico
+              </button>
+              <button className="text-[#fff] rounded-full hover:bg-[#fff] hover:text-[#6364F4] px-4 py-2">
+                Citas
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NavbarPaciente;
