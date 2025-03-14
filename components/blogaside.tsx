7ce290264191ea809057adcafa528d6f84bdc15b
marrScielo <mirthamarycielo@gmail.com>
2025-03-14 13:38:09 -0500

    avance 1

--- components/blogaside.tsx
+++ components/blogaside.tsx
@@ -1,60 +1,69 @@
-import { Avatar, Button, Divider } from "@heroui/react";
-
+import { Avatar, Button } from "@heroui/react";
 
 const categorias = [
-  { nombre: "Estrés" },
-  { nombre: "Trastorno Obsesivo Compulsivo (TOC)" },
-  { nombre: "Repulsión Sexual" },
-  { nombre: "Ansiedad" },
-  { nombre: "Depresión" },
-  { nombre: "Fobias" },
-  { nombre: "Trastorno de la Conducta Alimentaria" },
-  { nombre: "Trastorno de Estrés Postraumático (TEPT)" },
-  { nombre: "Trastornos del Sueño" },
-  { nombre: "Trastorno de Ansiedad Generalizada" },
-  { nombre: "Trastorno de Pánico" },
-  { nombre: "Trastorno Bipolar" },
   { nombre: "Adicciones" },
-  { nombre: "Trastorno de la Personalidad" },
-  { nombre: "Trastorno de la Conducta" },
+  { nombre: "Ansiedad" },
+  { nombre: "Atención" },
+  { nombre: "Autoestima" },
+  { nombre: "Crianza" },
+  { nombre: "Depresión" },
+  { nombre: "Enfermedades crónicas" },
+  { nombre: "Estrés" },
+  { nombre: "Impulsividad" },
+  { nombre: "TOC" },
+  { nombre: "Ira" },
+  { nombre: "Terapia de pareja" },
+  { nombre: "Sexualidad" },
+  { nombre: "Traumas" },
+  { nombre: "Riesgo suicida" },
+  { nombre: "Sentido de vida" },
+  { nombre: "Orientación vocacional" },
+  { nombre: "Problemas alimenticios" },
+  { nombre: "Relaciones interpersonales" },
 ];
-const autors = [
-  {  
-     imagen:"/logonombreblog.webp" ,
-     nombre: "Jhon Angelo Sánchez Garcia" 
-    },
-  
-  {      imagen:"/logonombreblog.webp" ,
 
-    nombre: "Jhon Angelo Sánchez Garcia" },
+const autores = [
+  {
+    imagen: "/logonombreblog.webp",
+    nombre: "Jhon Angelo Sánchez Garcia",
+  },
+  {
+    imagen: "/logonombreblog.webp",
+    nombre: "Jhon Angelo Sánchez Garcia",
+  },
 ];
+
 export default function BlogAside() {
   return (
     <div className="w-[400px] p-4">
-        <Divider orientation="vertical" />
-      <p className="text-xl font-semibold text-[#634AE2]">Categorías</p>
-      
-      {categorias.map((item, index) => (
+      {/* Sección de categorías */}
+      <p className="text-lg font-normal text-[#634AE2] mb-6">Categorías</p>
+      <div className="grid grid-cols-2 gap-2">
+        {categorias.map((item, index) => (
+          <Button
+            radius="full"
+            className={`bg-[#EAEAFF] text-base text-[#634AE2] hover:bg-[#C7B9FF] transition-all whitespace-nowrap ${
+              item.nombre.length > 15 ? "col-span-2" : ""
+            }`}
+            key={index}
+            style={{ maxWidth: "27vh" }} 
+          >
+            {item.nombre}
+          </Button>
+        ))}
+      </div>
+
+      <p className="text-base font-normal pt-7 m-4 text-[#634AE2]">Por autor</p>
+      {autores.map((item, index) => (
         <Button
           radius="full"
-          className="bg-[#EAEAFF] m-2 text-base text-[#634AE2] hover:bg-[#C7B9FF] transition-all"
+          className="bg-[#EAEAFF] pl-0.5 text-base text-[#634AE2] hover:bg-[#C7B9FF] transition-all"
           key={index}
         >
+          <Avatar className="" src={item.imagen} />
           {item.nombre}
         </Button>
       ))}
-      <p className="text-base font-normal pt-7 m-4 text-[#634AE2]">Por autor</p>
-      {autors.map((item, index) => (
-         <Button
-
-         radius="full"
-         className="bg-[#EAEAFF] pl-0.5 text-base text-[#634AE2] hover:bg-[#C7B9FF] transition-all"
-         key={index}
-       >
-       <Avatar className="" src={item.imagen}/>
-         {item.nombre}
-       </Button>
-    ))}      
-      </div>
+    </div>
   );
 }
