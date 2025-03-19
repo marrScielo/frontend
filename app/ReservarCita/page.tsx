9f6fa0dc44c74c2b066f3f7e3a259196108c1b5b
Toowaa <brahanax@gmail.com>
2025-03-19 09:56:13 -0500

    Fix buildeo

--- app/ReservarCita/page.tsx
+++ app/ReservarCita/page.tsx
@@ -1,13 +1,36 @@
-
-
-import ReservarComponents from "@/components/ReservarComponents"
+'use client';
+import { useEffect, useState } from "react";
+import ReservarComponents from "@/components/ReservarComponents";
 import { GetPsicologos } from "../apiRoutes";
+import { PsicologoApiResponse } from "@/interface";
 
-export default async function BlogPage() {
-  const Data= await GetPsicologos();
+export default function BlogPage() {
+
+  const [psicologos, setPsicologos] = useState<PsicologoApiResponse | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        setLoading(true);
+        const data = await GetPsicologos();
+        setPsicologos(data);
+      } catch (error) {
+        setError("Error obteniendo psicólogos");
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchData();
+  }, []);
+
   return (
-    <div >
-      <ReservarComponents  Psicologos={Data.result}/>
+    <div>
+      {loading && <p>Cargando...</p>}
+      {error && <p>{error}</p>}
+      {psicologos && <ReservarComponents Psicologos={psicologos.result} />}
     </div>
   );
 }
