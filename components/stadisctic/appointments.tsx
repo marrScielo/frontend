06489d08b452c9b8418e7ad80ab5ae7c2ad01872
marrScielo <mirthamarycielo@gmail.com>
2025-03-20 17:30:15 -0500

    diseño terminado de estadisticas clientes

--- components/stadisctic/appointments.tsx
+++ components/stadisctic/appointments.tsx
@@ -1,13 +1,20 @@
 export default function Appointments() {
   return (
-    <div className="grid xl:grid-cols-2 lg:grid-cols-1 m-5 place-items-center gap-5 max-w-[950px] mx-auto">
-      <div className="w-[547px] h-[459px] bg-white rounded-2xl flex items-center justify-center ">
+    <div className="grid xl:grid-cols-2 lg:grid-cols-1 m-5 place-items-center gap-5 max-w-[920px] mx-auto">
+      
+      <div className="w-[547px] h-[459px] bg-white rounded-2xl ">
+      <div className="rounded-r-full w-[247px] h-[60px] bg-[#6364F4] mt-6 flex items-center justify-center">
+          <p className="text-white font-medium text-start mr-10 text-xl">Citas totales <br/> del período: </p>
+        </div>
         <p className="text-black text-lg font-bold">Buenas</p>
       </div>
 
       {/* Segundo contenedor con dos cuadros internos */}
 
-      <div className="h-[459px] w-[353px] bg-white rounded-2xl flex items-center justify-center ">
+      <div className="h-[459px] w-[353px] bg-white rounded-2xl  ">
+      <div className="rounded-r-full w-[247px] h-[60px] bg-[#6364F4] mt-6 flex items-center justify-center">
+          <p className="text-white font-medium text-start mr-10 text-xl">Estado de <br/>cita:</p>
+        </div>
         <p className="text-black text-lg font-bold">Buenas</p>
       </div>
     </div>
--- components/stadisctic/clients.tsx
+++ components/stadisctic/clients.tsx
@@ -9,27 +9,32 @@ import {
   Bar,
   XAxis,
   YAxis,
-  CartesianGrid,
-  Legend,
 } from "recharts";
 
 // Datos para el gráfico de pastel
-const pieData = [
-  { name: "Desconocido", value: 20 },
-  { name: "Masculino", value: 40 },
-  { name: "Femenino", value: 40 },
+const genero = [
+  { name: "Desconocido", Total: 20 },
+  { name: "Masculino", Total: 40 },
+  { name: "Femenino", Total: 40 },
 ];
 
 const COLORS = ["#7777FF", "#66A3FF", "#B3B3FF"];
 
+const edad = [
+  { name: "0 - 12", Total: 10 },
+  { name: "13 - 17", Total: 5 },
+  { name: "18 - 24", Total: 17 },
+  { name: "25 - 34", Total: 12 },
+  { name: "35 - 44", Total: 6 },
+  { name: "45 - 54", Total: 7 },
+];
 
-const barData = [
-  { name: "0 - 12\naños", value: 10 },
-  { name: "13 - 17\naños", value: 5 },
-  { name: "18 - 24\naños", value: 17 },
-  { name: "15 - 34\naños", value: 12 },
-  { name: "35 - 44\naños", value: 6 },
-  { name: "45 - 54\naños", value: 7 }
+const lugar = [
+  { name: "Surco", Total: 10 },
+  { name: "Jesús María", Total: 5 },
+  { name: "Surquillo", Total: 17 },
+  { name: "Barranco", Total: 12 },
+  { name: "San Borja", Total: 6 },
 ];
 
 // Función para las etiquetas personalizadas en el gráfico de pastel
@@ -63,18 +68,18 @@ const renderCustomizedLabel = ({
 
 export default function Clients() {
   return (
-    <div className="grid xl:grid-cols-2 lg:grid-cols-1 m-5 place-items-center gap-5 max-w-[950px] mx-auto">
+    <div className="grid xl:grid-cols-2 lg:grid-cols-1 m-5 place-items-center gap-5 max-w-[920px] mx-auto">
       <div className="w-[401px] h-[600px] bg-white rounded-2xl flex flex-col">
         <div className="rounded-r-full w-[247px] h-[60px] bg-[#6364F4] mt-6 flex items-center justify-center">
-          <p className="text-white font-medium text-center mr-10">Género:</p>
+          <p className="text-white font-medium text-center mr-10 text-xl">Género:</p>
         </div>
 
-        <div className="w-full h-[400px] flex items-center justify-center">
+        <div className="w-full h-[350px] flex items-center justify-center">
           <ResponsiveContainer width="100%" height="100%">
             <PieChart>
               <Pie
-                dataKey="value"
-                data={pieData}
+                dataKey="Total"
+                data={genero}
                 cx="50%"
                 cy="50%"
                 innerRadius={70}
@@ -83,7 +88,7 @@ export default function Clients() {
                 label={renderCustomizedLabel}
                 labelLine={false}
               >
-                {pieData.map((entry, index) => (
+                {genero.map((entry, index) => (
                   <Cell
                     key={`cell-${index}`}
                     fill={COLORS[index % COLORS.length]}
@@ -94,31 +99,66 @@ export default function Clients() {
             </PieChart>
           </ResponsiveContainer>
         </div>
+
+        <div className="grid justify-start gap-5 mt-4 ml-14">
+          {genero.map((entry, index) => (
+            <div key={index} className="flex items-center gap-2">
+              <div
+                className="w-3 h-3 rounded-full"
+                style={{ backgroundColor: COLORS[index] }}
+              ></div>
+              <span className="text-[#634AE2] font-medium text-base">{entry.name}</span>
+            </div>
+          ))}
+        </div>
       </div>
 
-      {/* Segunda columna (Edad + gráfico de barras) */}
+      {/* Segunda columna  */}
       <div className="flex flex-col w-[502px] h-[600px] gap-5">
         <div className="w-full h-[300px] bg-white rounded-2xl flex flex-col">
           <div className="rounded-r-full w-[247px] h-[60px] bg-[#6364F4] mt-6 flex items-center justify-center">
-            <p className="text-white font-medium text-center mr-10">Edad:</p>
+            <p className="text-white font-medium text-center mr-10 text-xl">Edad:</p>
           </div>
 
-          <div className="flex-1 flex items-center justify-center">
-            <ResponsiveContainer width="90%" height="100%">
+          <div className="flex-1 flex items-center justify-center ">
+            <ResponsiveContainer width="90%" height="80%">
               <BarChart
-                data={barData}
-                margin={{ top: 20, right: 30, left: 10, bottom: 5 }}
+                data={edad}
+                margin={{ top: 15, right: 10, left: 5, bottom: 15 }}
               >
-                <CartesianGrid strokeDasharray="3 3" vertical={false} />
                 <XAxis
                   dataKey="name"
-                  tick={{ fontSize: 12, fill: "#666" }}
-                  tickLine={false}
+                  tickLine={{ stroke: "#634AE2" }}
+                  axisLine={{ stroke: "#634AE2" }}
+                  tick={({ x, y, payload }) => {
+                    return (
+                      <text
+                        x={x}
+                        y={y + 15}
+                        fill="#634AE2"
+                        textAnchor="middle"
+                        fontSize={12}
+                        fontWeight="500"
+                      >
+                        <tspan x={x} dy="0">
+                          {payload.value}
+                        </tspan>
+                        <tspan x={x} dy="15">
+                          años
+                        </tspan>{" "}
+                      </text>
+                    );
+                  }}
                 />
-                <YAxis tick={{ fontSize: 12, fill: "#666" }} tickLine={false} />
+
+                <YAxis
+                  tick={{ fontSize: 12, fill: "#634AE2" }}
+                  tickLine={{ stroke: "#634AE2" }}
+                  axisLine={{ stroke: "#634AE2" }}
+                />
                 <Tooltip />
                 <Bar
-                  dataKey="value"
+                  dataKey="Total"
                   fill="#9494F3"
                   barSize={35}
                   radius={[5, 5, 0, 0]}
@@ -129,10 +169,39 @@ export default function Clients() {
           </div>
         </div>
 
-        {/* Sección "Lugar" */}
-        <div className="w-full h-[300px] bg-white rounded-2xl">
+       
+        <div className="w-full h-[300px] bg-white rounded-2xl flex flex-col">
           <div className="rounded-r-full w-[247px] h-[60px] bg-[#6364F4] mt-6 flex items-center justify-center">
-            <p className="text-white font-medium text-center mr-10">Lugar:</p>
+            <p className="text-white font-medium text-center mr-10 text-xl">Lugar:</p>
+          </div>
+
+          <div className="flex-1 flex items-center justify-center">
+            <ResponsiveContainer width="90%" height="80%" >
+              <BarChart
+                data={lugar}
+                margin={{ top: 15, right: 10, left: 5, bottom: 15 }}
+              >
+                <XAxis
+                  dataKey="name"
+                  tickLine={{ stroke: "#634AE2" }}
+                  axisLine={{ stroke: "#634AE2" }}
+                  tick={{ fontSize: 12, fill: "#634AE2", fontWeight: "500" }}
+                />
+                <YAxis
+                  tick={{ fontSize: 12, fill: "#634AE2" }}
+                  tickLine={{ stroke: "#634AE2" }}
+                  axisLine={{ stroke: "#634AE2" }}
+                />
+                <Tooltip />
+                <Bar
+                  dataKey="Total"
+                  fill="#9494F3"
+                  barSize={35}
+                  radius={[5, 5, 0, 0]}
+                  opacity={0.6}
+                />
+              </BarChart>
+            </ResponsiveContainer>
           </div>
         </div>
       </div>
