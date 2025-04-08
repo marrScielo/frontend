b4587fdb81c5e84607ff8eef676faab3092ade20
Toowaa <brahanax@gmail.com>
2025-04-08 12:08:11 -0500

    Refactor DashboardResumen component for improved layout and responsiveness

--- components/User/Dashboard/DashboardResumen.tsx
+++ components/User/Dashboard/DashboardResumen.tsx
@@ -4,13 +4,13 @@ import React from "react";
 export default function DashboardResumen() {
   return (
     <>
-      <div className="space-y-6">
+      <div className="space-y-6 max-w-xl w-full">
         <div className="bg-[#fff] rounded-3xl pt-8">
           <div className="flex rounded-r-full py-6 text-[#fff] bg-[#6364F4] justify-center font-normal text-2xl w-3/4 ">
             Resumen
           </div>
-          <div>
-            <ul className="list-disc pl-7 text-[#634AE2] text-xl font-normal ml-28 p-6">
+          <div className="mx-10 sm:mx-14 md:mx-20 lg:mx-24">
+            <ul className="list-disc pl-7 text-[#634AE2] text-xl font-normal   p-6">
               <li> 0 citas completadas</li>
               <li> 7 citas pendientes</li>
               <li> 0 Citas canceladas</li>
