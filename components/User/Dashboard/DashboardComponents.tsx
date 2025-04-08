8ed9ab2002009ce6bab0d1af8f10b1543b41f516
Toowaa <brahanax@gmail.com>
2025-04-08 12:08:14 -0500

    Refactor DashboardComponents for improved layout and responsiveness

--- components/User/Dashboard/DashboardComponents.tsx
+++ components/User/Dashboard/DashboardComponents.tsx
@@ -5,15 +5,10 @@ import VirtualizedTable from "./DashboardCitas";
 
 export default function DashboardComponents() {
   return (
-    <>
-      <div className="grid grid-cols-2 gap-4 pt-12">
-        <div className="w-4/6 ml-8">
-          <DashboardResumen />
-        </div>
-        <div className="-ml-40 ">
-          <VirtualizedTable/>
-        </div>
-      </div>
-    </>
+    <section className="flex flex-col md:flex-row mx-5   gap-10 ">
+      <DashboardResumen/>
+      <VirtualizedTable/>
+    </section>
   );
 }
+
