f6f8bcdc1d5f4b0f7c0a25f68fdbaf73d3c30850
FranksVilca <franksjaviervilcaquispe@gmail.com>
2025-04-11 11:35:43 -0500

    feat(DetallePaciente):Se rellena ya los datos automaticamente

--- components/User/Historial/DetallesPaciente.tsx
+++ components/User/Historial/DetallesPaciente.tsx
@@ -1,5 +1,6 @@
-import { AtencionFormData, DatePacienteProps, DetallesAtencionProps, ListaCitas } from "@/interface";
-import React from "react";
+import { AtencionFormData, DetallesAtencionProps } from "@/interface";
+import { parseCookies } from "nookies";
+import React, { useEffect, useState } from "react";
 import { useDropzone } from "react-dropzone";
 
 function DropzoneWithoutKeyboard() {
@@ -23,7 +24,38 @@ function DropzoneWithoutKeyboard() {
   );
 }
 
-export const DetallesPaciente: React.FC<DetallesAtencionProps> = ({ idAtencion }) => {
+export const DetallesPaciente: React.FC<DetallesAtencionProps> = ({
+  idAtencion,
+}) => {
+  const [atencionPaciente, setAtencionPaciente] =
+    useState<AtencionFormData | null>(null);
+
+  const handleGetAtencion = async () => {
+    try {
+      const cookies = parseCookies();
+      const token = cookies["session"];
+      const url = `${process.env.NEXT_PUBLIC_API_URL}api/atenciones/${idAtencion}`;
+      const response = await fetch(url, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      const data = await response.json();
+      if (response.ok && data.result) {
+        setAtencionPaciente(data.result[0]);
+      }
+    } catch (error) {
+      console.error("Error al obtener atención específica:", error);
+    }
+  };
+
+  useEffect(() => {
+    handleGetAtencion();
+  });
+
   return (
     <div className="max-w-[480px]">
       <div className="bg-[#fff] h-max text-[#634AE2] rounded-3xl">
@@ -36,21 +68,25 @@ export const DetallesPaciente: React.FC<DetallesAtencionProps> = ({ idAtencion }
           <textarea
             className="bg-[#F3F3F3] w-full h-20 border-1 font-light text-[#634AE2] p-3 rounded-3xl placeholder:text-[#634AE2] text-base"
             placeholder="Aun no hay comentarios"
+            value={atencionPaciente?.Diagnostico}
           />
           <div className="pb-1">Tratamiento</div>
           <textarea
             className="bg-[#F3F3F3] w-full h-20 border-1 font-light text-[#634AE2] p-3 rounded-3xl placeholder:text-[#634AE2] text-base"
             placeholder="Aun no hay comentarios"
+            value={atencionPaciente?.Tratamiento}
           />
           <div className="pb-1">Observación</div>
           <textarea
             className="bg-[#F3F3F3] w-full h-20 border-1 font-light text-[#634AE2] p-3 rounded-3xl placeholder:text-[#634AE2] text-base"
             placeholder="Aun no hay comentarios"
+            value={atencionPaciente?.Observacion}
           />
           <div className="pb-1">Objetivo alcanzados</div>
           <textarea
             className="bg-[#F3F3F3] w-full h-20 border-1 font-light text-[#634AE2] p-3 rounded-3xl placeholder:text-[#634AE2] text-base"
             placeholder="Aun no hay comentarios"
+            value={atencionPaciente?.Observacion}
           />
           <div className="pb-1">Documento adicionales</div>
         </div>
