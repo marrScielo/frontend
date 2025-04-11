d78cba95efbcc64360cc1e33f354aceb57c289dd
Toowaa <brahanax@gmail.com>
2025-04-11 16:49:29 -0500

    fix(ReservarPsiPreview): adjust Card component width for better responsiveness

--- components/ReservarPsiPreview.tsx
+++ components/ReservarPsiPreview.tsx
@@ -130,7 +130,7 @@ export default function ReservarPsiPreview({
 
   return (
     <>
-      <Card className="w-full bg-background p-5 rounded-3xl border-[#9494F3] border-t-[0.5px]">
+      <Card className="w-full md:max-w-md bg-background p-5 rounded-3xl border-[#9494F3]">
         <div>
           <div className="w-full grid grid-cols-1 sm:grid-cols-3 gap-4">
             <div className="col-span-1 flex sm:justify-start">
