bd07290964c0b049f428198572d1432364841afc
Toowaa <brahanax@gmail.com>
2025-04-11 16:49:32 -0500

    refactor(ReservarComponents): enhance layout and improve search functionality

--- components/ReservarComponents.tsx
+++ components/ReservarComponents.tsx
@@ -1,20 +1,55 @@
-'use client'
+"use client";
 import { PsicologoPreviewData } from "@/interface";
 import ReservarComponentSearch from "./ReservarComponentSearch";
 import ReservarPsiPreview from "./ReservarPsiPreview";
 import { useState } from "react";
 
-export default function ReservarComponents({ Psicologos }: { Psicologos: PsicologoPreviewData[] }) {
-  const [searchTerm, setSearchTerm] = useState('');
+export default function ReservarComponents({
+  Psicologos,
+}: {
+  Psicologos: PsicologoPreviewData[];
+}) {
+  const [searchTerm, setSearchTerm] = useState("");
 
- 
-  const filteredPsicologos = Psicologos.filter(psicologo => {
+  const filteredPsicologos = Psicologos.filter((psicologo) => {
     const fullName = `${psicologo.nombre} ${psicologo.apellido}`.toLowerCase();
     return fullName.includes(searchTerm.toLowerCase());
   });
 
   return (
-    <div className="flex justify-center text-[#634AE2]">
+    <div className="w-full md:max-w-7xl mx-auto " >
+      <div className="  text-[#634AE2] mx-auto  py-5 md:py-10">
+        <h1 className="pt-10 lg:text-3xl font-bold text-center md:text-start text-2xl">
+          La mejor inversión es en
+          <br className="block sm:hidden" /> ti mismo
+          <br className="block sm:hidden" /> ¡Comienza tu proceso hoy!
+        </h1>
+
+        <h5 className="lg:mt-3 font-light lg:text-[24px] text-center md:text-start text-base mt-1">
+          Agenda tu sesión con un psicólogo en
+          <br className="block sm:hidden" /> línea, fácil, seguro y privado
+        </h5>
+      </div>
+      <div className="flex flex-col sm:flex-row gap-4 pb-6">
+        <div>
+          <ReservarComponentSearch
+            onSearchChange={(term) => setSearchTerm(term)}
+          />
+        </div>
+        <div className="flex flex-wrap gap-8">
+          {filteredPsicologos.map((Item, index) => (
+            <div key={index} className="w-full sm:w-auto mx-auto">
+              <ReservarPsiPreview psicologo={Item} />
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+/*
+ <div className="flex justify-center text-[#634AE2]">
       <div className="w-full max-w-7xl">
         <h1 className="lg:text-start pt-10 lg:text-3xl font-bold text-center text-2xl">
           La mejor inversión es en
@@ -26,7 +61,10 @@ export default function ReservarComponents({ Psicologos }: { Psicologos: Psicolo
           Agenda tu sesión con un psicólogo en
           <br className="block lg:hidden" /> línea, fácil, seguro y privado
         </h5>
-        <div className="flex justify-center mt-8 px-4">
+    
+      </div>
+    </div>
+    <div className="flex justify-center mt-8 px-4">
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-5 w-full ">
             <div className="col-span-1 sm:row-span-5 sm:col-span-1 ">
               <ReservarComponentSearch 
@@ -44,7 +82,6 @@ export default function ReservarComponents({ Psicologos }: { Psicologos: Psicolo
             </div>
           </div>
         </div>
-      </div>
-    </div>
-  );
-}
+
+
+*/
